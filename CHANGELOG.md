14f6125484362b87d7070fff01389a62ae5c7ab0
Jose Armesto <github@armesto.net>
2022-02-02 13:02:45 +0100

    Add deploy component label (#29)
    * Add component label to Deployments
    
    * Improve github PR template
    
    * Remove unnecesary files
    
    * Fix deployments images
    
    * Remove unnecesary files using kustomize
    
    * Update CHANGELOG.md
    
    Co-authored-by: Paweł Kopiczko <pawel@giantswarm.io>
    
    * Remove spaces from images to avoid kustomize bug
    
    * Added make verify to make sure manifests are up to date
    
    * Add Github action to verify manifests
    
    Co-authored-by: Paweł Kopiczko <pawel@giantswarm.io>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- Add component label to Deployments.
+- Add `make verify` to make sure manifests are up to date.
+
 ## [1.0.0] - 2022-01-27
 
 ### Changed
--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -2,3 +2,11 @@
 generate:
 	kustomize build config/helm -o helm/cluster-api/templates
 	rm -rf helm/cluster-api/templates/apiextensions* helm/cluster-api/templates/cert-manager.io_v1_issuer*
+
+
+.PHONY: verify
+verify: generate
+	@if !(git diff --quiet HEAD); then \
+		git diff; \
+		echo "generated files are out of date, run make generate"; exit 1; \
+	fi
