61619f6eee8dba7259fb591f0a17a66d1311421a
Jose Armesto <github@armesto.net>
2022-03-03 16:41:05 +0100

    Don't use kustomize for pre-install hook

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -1,8 +1,8 @@
 .PHONY: generate
 generate:
-	rm -rf helm/cluster-api/templates/*
+	@rm -rf helm/cluster-api/templates/*.yaml
 	kustomize build config/helm -o helm/cluster-api/templates
-	rm -rf helm/cluster-api/templates/apiextensions* helm/cluster-api/templates/cert-manager.io_v1_issuer*
+	@rm -rf helm/cluster-api/templates/apiextensions* helm/cluster-api/templates/cert-manager.io_v1_issuer*
 
 
 .PHONY: verify
