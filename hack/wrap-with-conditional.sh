a91b785a10e427ad2a2f22bad9f9fe3fca74c563
Paweł Kopiczko <kopiczko@gmail.com>
2023-05-15 19:22:50 +0100

    Cosmetic 2

--- hack/wrap-with-conditional.sh
+++ hack/wrap-with-conditional.sh
@@ -6,11 +6,10 @@ set -o pipefail
 
 cd "helm/cluster-api/templates"
 
-readonly match="  objectSelector:\r    matchLabels:\r      cluster.x-k8s.io\/watch-filter: '{{ .Values.watchFilter }}'"
-
 for file in admissionregistration.k8s.io_v1_*.yaml; do
-  new_content=$(tr '\n' '\r' < "${file}" | sed -e "s/${match}/{{ if .Values.watchFilter }}\n${match}\n{{ end }}/g" | tr '\r' '\n')
-  printf "%s\n" "${new_content}" > "${file}"
+    match="  objectSelector:\r    matchLabels:\r      cluster.x-k8s.io\/watch-filter: '{{ .Values.watchFilter }}'"
+    new_content=$(tr '\n' '\r' < "${file}" | sed -e "s/${match}/{{ if .Values.watchFilter }}\n${match}\n{{ end }}/g" | tr '\r' '\n')
+    printf "%s\n" "${new_content}" > "${file}"
 done
 
 for file in *_ciliumnetworkpolicy_*.yaml; do
