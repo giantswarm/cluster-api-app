03e5970da648c5bd717a0af0258e0d88aae11b8e
Marco Ebert <marco_ebert@icloud.com>
2024-01-05 18:38:29 +0100

    Repository: Chores all over the place. (#172)
    * ABS: Rework `main.yaml`.
    
    * CircleCI: Rework `config.yml`.
    
    * GitHub: Remove `pull_request_template.md`.
    
    * Chart: Rework `Chart.yaml` & remove `ci/`.
    
    * GitHub: Rework `verify.yaml`.
    
    * Make: Rework targets.
    
    * Git: Update `.gitignore`.
    
    * Nancy: Remove `.nancy-ignore` & `.nancy-ignore.generated`.
    
    * README: Update according to changes.

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -1,36 +1,16 @@
-.PHONY: generate
-generate: ## Generate kustomize patches and all helm charts
-	./hack/fetch-manifest.sh
-	$(MAKE) delete-generated-helm-charts
-	kubectl kustomize config/helm -o helm/cluster-api/templates
-	rm -v helm/cluster-api/templates/v1_configmap_watchfilter-patch.yaml
-	./hack/move-generated-crds.sh
-	./hack/generate-crd-version-patches.sh
-	./hack/wrap-with-conditional.sh
+.PHONY: generate verify
 
-delete-generated-helm-charts:
-	@rm -rf helm/cluster-api/templates/*.yaml
+generate:
+	rm helm/cluster-api/templates/*.yaml
+	hack/fetch-manifest.sh
+	kubectl kustomize config/helm --output helm/cluster-api/templates
+	rm helm/cluster-api/templates/v1_configmap_watchfilter-patch.yaml
+	hack/move-generated-crds.sh
+	hack/generate-crd-version-patches.sh
+	hack/wrap-with-conditional.sh
 
-CRD_BUILD_DIR := out
-
-$(CRD_BUILD_DIR):
-	mkdir -p $(CRD_BUILD_DIR)/
-
-.PHONY: release-manifests
-release-manifests: $(CRD_BUILD_DIR) ## Builds the manifests to publish with a release
-	# Build core-components.
-	kubectl kustomize config/helm/files > $(CRD_BUILD_DIR)/crds.yaml
-
-.PHONY: verify
 verify: generate
-	@if !(git diff --quiet HEAD); then \
-		git diff; \
-		echo "generated files are out of date, run make generate"; exit 1; \
+	@if ! git diff --exit-code ; then \
+		echo "Generated templates are out of date. Run make generate." ; \
+		exit 1 ; \
 	fi
-
-ensure-schema-gen:
-	@helm schema-gen --help &>/dev/null || helm plugin install https://github.com/mihaisee/helm-schema-gen.git
-
-.PHONY: schema-gen
-schema-gen: ensure-schema-gen ## Generates the values schema file
-	@cd helm/cluster-api && helm schema-gen values.yaml > values.schema.json && git diff values.schema.json
