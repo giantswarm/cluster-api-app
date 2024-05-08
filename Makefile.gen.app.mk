1c87adbfc4a5c755a1e3eebf033e4db74d40d085
Nick Jüttner <nick@giantswarm.io>
2024-05-08 12:20:23 +0200

    CAPI 1.5.7

--- Makefile.gen.app.mk
+++ Makefile.gen.app.mk
@@ -16,14 +16,14 @@ endif
 
 .PHONY: lint-chart check-env update-chart helm-docs update-deps $(DEPS)
 
-lint-chart: IMAGE := giantswarm/helm-chart-testing:v3.11.0
+lint-chart: IMAGE := giantswarm/helm-chart-testing:v3.0.0-rc.1
 lint-chart: check-env ## Runs ct against the default chart.
 	@echo "====> $@"
 	rm -rf /tmp/$(APPLICATION)-test
 	mkdir -p /tmp/$(APPLICATION)-test/helm
 	cp -a ./helm/$(APPLICATION) /tmp/$(APPLICATION)-test/helm/
 	architect helm template --dir /tmp/$(APPLICATION)-test/helm/$(APPLICATION)
-	docker run -it --rm -v /tmp/$(APPLICATION)-test:/wd --platform=linux/arm64/v8 --workdir=/wd --name ct $(IMAGE) ct lint --validate-maintainers=false --charts="helm/$(APPLICATION)"
+	docker run -it --rm -v /tmp/$(APPLICATION)-test:/wd --workdir=/wd --name ct $(IMAGE) ct lint --validate-maintainers=false --charts="helm/$(APPLICATION)"
 	rm -rf /tmp/$(APPLICATION)-test
 
 update-chart: check-env ## Sync chart with upstream repo.
