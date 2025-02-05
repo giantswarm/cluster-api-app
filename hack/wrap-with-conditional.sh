d1669d7395be2042d2d03fef85753af141106213
Marco Ebert <marco_ebert@icloud.com>
2025-02-05 12:09:46 +0100

    Chart: Replace Cilium network policies by native ones. (#244)

--- hack/wrap-with-conditional.sh
+++ hack/wrap-with-conditional.sh
@@ -11,10 +11,3 @@ for file in admissionregistration.k8s.io_v1_*.yaml; do
     new_content=$(tr '\n' '\r' < "${file}" | sed -e "s/${match}/{{ if .Values.watchFilter }}\n${match}\n{{ end }}/g" | tr '\r' '\n')
     printf "%s\n" "${new_content}" > "${file}"
 done
-
-for file in *_ciliumnetworkpolicy_*.yaml; do
-    data=$(cat "${file}")
-    echo '{{- if .Values.ciliumNetworkPolicy.enabled }}' > "${file}"
-    echo "${data}" >> "${file}"
-    echo '{{- end }}' >> "${file}"
-done
